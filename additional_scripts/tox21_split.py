e7096fdedb8272095c650fd824ebe9f8b5f5146b
joXemMx <63006948+joXemMx@users.noreply.github.com>
2022-10-12 17:52:56 +0200

    save without row indexing

--- additional_scripts/tox21_split.py
+++ additional_scripts/tox21_split.py
@@ -11,18 +11,18 @@ os.mkdir('../data/tox21_split/')
 
 test_idx = pd.read_csv(splits_path+'test.csv.gz', compression='gzip').to_numpy().flatten()
 test_data = pd.concat([data.iloc[[k]] for k in test_idx])
-test_data.to_csv('../data/tox21_split/tox21_test.csv')
+test_data.to_csv('../data/tox21_split/tox21_test.csv', index=False)
 del test_idx
 del test_data
 
 train_idx = pd.read_csv(splits_path+'train.csv.gz', compression='gzip').to_numpy().flatten()
 train_data = pd.concat([data.iloc[[k]] for k in train_idx])
-train_data.to_csv('../data/tox21_split/tox21_train.csv')
+train_data.to_csv('../data/tox21_split/tox21_train.csv', index=False)
 del train_idx
 del train_data
 
 val_idx = pd.read_csv(splits_path+'valid.csv.gz', compression='gzip').to_numpy().flatten()
 val_data = pd.concat([data.iloc[[k]] for k in val_idx])
-val_data.to_csv('../data/tox21_split/tox21_validation.csv')
+val_data.to_csv('../data/tox21_split/tox21_validation.csv', index=False)
 del val_idx
 del val_data
