28919c16d74b546ffdee2ee351877443e3b0d51d
joXemMx <63006948+joXemMx@users.noreply.github.com>
2022-10-13 21:10:47 +0200

    back to working

--- additional_scripts/check_equal.py
+++ additional_scripts/check_equal.py
@@ -2,8 +2,8 @@
 import pandas as pd
 
 tpp_data = pd.read_csv('/beegfs/lo63tor/graphtrans/data/ogbg_moltox21/mapping/mol.csv.gz', compression='gzip').drop(columns=['mol_id'])
-#tpp_smiles = tpp_data.pop('smiles')
-#tpp_data.insert(0, 'smiles', tpp_smiles)
+tpp_smiles = tpp_data.pop('smiles')
+tpp_data.insert(0, 'smiles', tpp_smiles)
 cp_data = pd.read_csv('../data/tox21.csv')
 
 print('tox21 datasets of ToxPredProjekt and chemprop are equal') if tpp_data.equals(cp_data) else print('tox21 datasets of ToxPredProjekt and chemprop are NOT equal')
