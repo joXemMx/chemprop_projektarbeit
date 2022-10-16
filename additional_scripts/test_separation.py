897385ba638f5ca5b342afbe2f496e1ae7224547
joXemMx <63006948+joXemMx@users.noreply.github.com>
2022-10-16 15:22:17 +0200

    change save path

--- additional_scripts/test_separation.py
+++ additional_scripts/test_separation.py
@@ -8,7 +8,7 @@ data = pd.read_csv(data_path)
 #data = pd.read_csv(data_path+'mol.csv.gz', compression='gzip')
 splits_path = '/beegfs/lo63tor/graphtrans/data/ogbg_moltox21/split/scaffold/'
 
-os.mkdir('../data/tox21_split/')
+os.mkdir('../data/tox21_test_separation/')
 
 test_idx = pd.read_csv(splits_path+'test.csv.gz', compression='gzip').to_numpy().flatten()
 test_data = pd.concat([data.iloc[[k]] for k in test_idx])
@@ -20,4 +20,4 @@ train_idx = pd.read_csv(splits_path+'train.csv.gz', compression='gzip').to_numpy
 val_idx = pd.read_csv(splits_path+'valid.csv.gz', compression='gzip').to_numpy().flatten()
 train_val_idx = np.concatenate([train_idx, val_idx])
 train_val = pd.concat([data.iloc[[k]] for k in train_val_idx])
-train_val.to_csv('../data/tox21_split/tox21_train_val.csv', index=False)
+train_val.to_csv('../data/tox21_test_separation/tox21_train_val.csv', index=False)
