47e4040f1b0d9ad883bd6d32e95720d48e48b162
joXemMx <vo87poq@login01.ara>
2022-10-16 15:44:01 +0200

    add slurm + update commands.txt

--- chemprop_slurm.sh
+++ chemprop_slurm.sh
@@ -6,4 +6,4 @@
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate chemprop
-python /home/vo87poq/chemprop_projektarbeit/train.py --data_path /home/vo87poq/chemprop_projektarbeit/data/tox21.csv --dataset_type classification --save_dir /home/vo87poq/chemprop_projektarbeit/tox21_checkpoints/standard_gpu --split_type scaffold_balanced --quiet
+python /home/vo87poq/chemprop_projektarbeit/train.py --data_path /home/vo87poq/chemprop_projektarbeit/data/tox21_test_separation/tox21_train_val.csv --dataset_type classification --save_dir /home/vo87poq/chemprop_projektarbeit/tox21_checkpoints/noTest_10fold_gpu --split_type scaffold_balanced --num_folds 10 --quiet
