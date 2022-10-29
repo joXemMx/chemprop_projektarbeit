24f6d33ae66cda889f3413a4d1e36db1fae4bb7c
joXemMx <63006948+joXemMx@users.noreply.github.com>
2022-10-29 17:30:09 +0200

    add chemprop split indexing

--- additional_scripts/tox21_split.py
+++ additional_scripts/tox21_split.py
@@ -5,24 +5,49 @@ data_path = '../data/tox21.csv'
 data = pd.read_csv(data_path)
 #data_path = '/beegfs/lo63tor/graphtrans/data/ogbg_moltox21/mapping'
 #data = pd.read_csv(data_path+'mol.csv.gz', compression='gzip')
-splits_path = '/beegfs/lo63tor/graphtrans/data/ogbg_moltox21/split/scaffold/'
 
-os.mkdir('../data/tox21_split/')
+#splits_path = '/beegfs/lo63tor/graphtrans/data/ogbg_moltox21/split/scaffold/'
+splits_path = '../data/tox21_split_chemprop/'
 
-test_idx = pd.read_csv(splits_path+'test.csv.gz', compression='gzip').to_numpy().flatten()
-test_data = pd.concat([data.iloc[[k]] for k in test_idx])
-test_data.to_csv('../data/tox21_split/tox21_test.csv', index=False)
-del test_idx
-del test_data
+if (splits_path == '../data/tox21_split_chemprop/'):
+  
+  test_idx = pd.read_csv(splits_path+'tox21_test_idx.csv').to_numpy().flatten()
+  test_data = pd.concat([data.iloc[[k]] for k in test_idx])
+  test_data.to_csv('../data/tox21_split_chemprop/tox21_test.csv', index=False)
+  del test_idx
+  del test_data
 
-train_idx = pd.read_csv(splits_path+'train.csv.gz', compression='gzip').to_numpy().flatten()
-train_data = pd.concat([data.iloc[[k]] for k in train_idx])
-train_data.to_csv('../data/tox21_split/tox21_train.csv', index=False)
-del train_idx
-del train_data
+  train_idx = pd.read_csv(splits_path+'tox21_train_idx.csv').to_numpy().flatten()
+  train_data = pd.concat([data.iloc[[k]] for k in train_idx])
+  train_data.to_csv('../data/tox21_split_chemprop/tox21_train.csv', index=False)
+  del train_idx
+  del train_data
 
-val_idx = pd.read_csv(splits_path+'valid.csv.gz', compression='gzip').to_numpy().flatten()
-val_data = pd.concat([data.iloc[[k]] for k in val_idx])
-val_data.to_csv('../data/tox21_split/tox21_validation.csv', index=False)
-del val_idx
-del val_data
+  val_idx = pd.read_csv(splits_path+'tox21_valid_idx.csv').to_numpy().flatten()
+  val_data = pd.concat([data.iloc[[k]] for k in val_idx])
+  val_data.to_csv('../data/tox21_split_chemprop/tox21_validation.csv', index=False)
+  del val_idx
+  del val_data
+  
+else:
+  
+  if not (os.path.isdir('../data/tox21_split/')):
+    os.mkdir('../data/tox21_split/')
+
+  test_idx = pd.read_csv(splits_path+'test.csv.gz', compression='gzip').to_numpy().flatten()
+  test_data = pd.concat([data.iloc[[k]] for k in test_idx])
+  test_data.to_csv('../data/tox21_split/tox21_test.csv', index=False)
+  del test_idx
+  del test_data
+
+  train_idx = pd.read_csv(splits_path+'train.csv.gz', compression='gzip').to_numpy().flatten()
+  train_data = pd.concat([data.iloc[[k]] for k in train_idx])
+  train_data.to_csv('../data/tox21_split/tox21_train.csv', index=False)
+  del train_idx
+  del train_data
+
+  val_idx = pd.read_csv(splits_path+'valid.csv.gz', compression='gzip').to_numpy().flatten()
+  val_data = pd.concat([data.iloc[[k]] for k in val_idx])
+  val_data.to_csv('../data/tox21_split/tox21_validation.csv', index=False)
+  del val_idx
+  del val_data
