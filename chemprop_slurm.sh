df1bf5b0bc8f8f2532bd88a620cdf1ba200f185c
joXemMx <vo87poq@login01.ara>
2022-10-31 12:17:10 +0100

    added example slurm

--- chemprop_slurm.sh
+++ chemprop_slurm.sh
@@ -2,8 +2,10 @@
 #SBATCH --partition=gpu_p100,gpu_v100,gpu_a100
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
-#SBATCH --mem-per-cpu=16000
+#SBATCH --mem-per-cpu=32000
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate chemprop
-python /home/vo87poq/chemprop_projektarbeit/train.py --data_path /home/vo87poq/chemprop_projektarbeit/data/tox21_test_separation/tox21_train_val.csv --dataset_type classification --save_dir /home/vo87poq/chemprop_projektarbeit/tox21_checkpoints/noTest_10fold_gpu --split_type scaffold_balanced --num_folds 10 --quiet
+#python hyperparameter_optimization.py --data_path /home/vo87poq/chemprop_projektarbeit/data/tox21_split/tox21_train.csv --dataset_type classification --epochs=30 --seed=0 --num_iters=300 --split_type scaffold_balanced --config_save_path /home/vo87poq/chemprop_projektarbeit/data/tox21_split/config_300.txt --log_dir /home/vo87poq/chemprop_projektarbeit/data/tox21_split/logs_300
+#python /home/vo87poq/chemprop_projektarbeit/hyperparameter_optimization.py --data_path /home/vo87poq/chemprop_projektarbeit/data/tox21.csv --dataset_type classification --epochs 30 --num_folds 10 --num_iters=20 --split_type scaffold_balanced --config_save_path /home/vo87poq/chemprop_projektarbeit/data/tox21_split/config_cv.txt --log_dir /home/vo87poq/chemprop_projektarbeit/data/tox21_split/logs_cv
+python /home/vo87poq/chemprop_projektarbeit/train.py --data_path /home/vo87poq/chemprop_projektarbeit/data/tox21.csv --dataset_type classification --save_dir /home/vo87poq/chemprop_projektarbeit/tox21_checkpoints/standard_run --split_type scaffold_balanced --quiet
