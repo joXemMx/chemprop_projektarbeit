598d149c2ad7b1c0f8b20741f038bb93a23fb4fb
joXemMx <jonas.em@t.online.de>
2023-01-14 11:18:57 +0100

    update script

--- additional_scripts/ogb_split.py
+++ additional_scripts/ogb_split.py
@@ -2,34 +2,34 @@ import pandas as pd
 import os
 
 # use pre-installed tox21 data set 
-data_path = os.getcwd()+'/data/'
-data = pd.read_csv(data_path+ 'tox21.csv')
+path = os.getcwd()
+data = pd.read_csv(path+ '/data/tox21.csv')
 
 # alternative to use the data set of GraphTrans
-# data_path = '/beegfs/lo63tor/graphtrans/data/ogbg_moltox21/mapping/'
-# data = pd.read_csv(data_path+'mol.csv.gz', compression='gzip')
+# path = '/beegfs/lo63tor/graphtrans/data/ogbg_moltox21/mapping/'
+# data = pd.read_csv(path+'mol.csv.gz', compression='gzip')
 
 # read split index files from OGB
 splits_path = '/beegfs/lo63tor/graphtrans/data/ogbg_moltox21/split/scaffold/'
   
-if not (os.path.isdir(data_path+ 'tox21_split/')):
-  os.mkdir(data_path+'tox21_split/')
+if not (os.path.isdir(path+ '/newdata/tox21_split_ogb/')):
+  os.mkdir(path+ '/newdata/tox21_split_ogb/')
 
-# save the data sets generated when appliying the split index files
+# save the data sets generated when applying the split index files
 test_idx = pd.read_csv(splits_path+'test.csv.gz', compression='gzip').to_numpy().flatten()
 test_data = pd.concat([data.iloc[[k]] for k in test_idx])
-test_data.to_csv( data_path + 'newdata/tox21_split_ogb/ogb_test.csv', index=False)
+test_data.to_csv(path + 'newdata/tox21_split_ogb/ogb_test.csv', index=False)
 del test_idx
 del test_data
 
 train_idx = pd.read_csv(splits_path+'train.csv.gz', compression='gzip').to_numpy().flatten()
 train_data = pd.concat([data.iloc[[k]] for k in train_idx])
-train_data.to_csv( data_path + 'newdata/tox21_split_ogb/ogb_train.csv', index=False)
+train_data.to_csv(path + 'newdata/tox21_split_ogb/ogb_train.csv', index=False)
 del train_idx
 del train_data
 
 val_idx = pd.read_csv(splits_path+'valid.csv.gz', compression='gzip').to_numpy().flatten()
 val_data = pd.concat([data.iloc[[k]] for k in val_idx])
-val_data.to_csv(data_path + 'newdata/tox21_split_ogb/ogb_val.csv', index=False)
+val_data.to_csv(path + 'newdata/tox21_split_ogb/ogb_val.csv', index=False)
 del val_idx
 del val_data
