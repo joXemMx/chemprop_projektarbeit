d50bdd5b2114f6884040c3190acb18d599062aaf
joXemMx <jonas.em@t.online.de>
2023-03-21 21:53:38 +0100

    all final plots

--- DensityPlot/densityGen.py
+++ DensityPlot/densityGen.py
@@ -70,113 +70,77 @@ for label in labels:
     print("hit:",len(x))
     print("no hit:",len(y))
     
-    # print("roc:",roc_auc_score(res[label,"Real Value"][~np.isnan(res[label,"Real Value"])], res[label,"Prediction"][~np.isnan(res[label,"Real Value"])]))
-    # print("roc with zero:",roc_auc_score(res[label,"Real Value"][~np.isnan(res[label,"Real Value"])],zero_arr[~np.isnan(res[label,"Real Value"])]))
+    # # double hist
     # fig, (ax1, ax2) = plt.subplots(nrows=2)
     # def log_freq(weights):
     #     return np.log10(weights)
     # sns.histplot(x, bins=20, binrange=[0,1], alpha=0.5, color='red', ax=ax1, log_scale=(False,False))
     # sns.histplot(y, bins=20, binrange=[0,1], alpha=0.5, color='blue', ax=ax2, log_scale=(False,False))
     # fig.subplots_adjust(hspace=0.5)
-    # ax1.set_ylabel('Frequency')
+    # ax1.set_ylabel('Count')
     # ax1.set_xlabel('Predictions for toxic')
     # ax2.set_ylabel('Frequency')
     # ax2.set_xlabel('Predictions for non-toxic')
-    # plt.suptitle('Histogram of Two Datasets')
+    # plt.suptitle('Amount of predictions dependent on value')
+    # plt.savefig('double_hist/hist_'+label+'.png')
+    # plt.close()
 
+    # # precision recall curve
     # arr = res[label,"Real Value"][~np.isnan(res[label,"Real Value"])]
-    # print(arr)
     # percentile = sum(arr) / len(arr)
-    # print(percentile)
-    # print(sum(arr))
     # precision, recall, thresholds = precision_recall_curve(y_true = res[label,"Real Value"][~np.isnan(res[label,"Real Value"])], probas_pred = res[label,"Prediction"][~np.isnan(res[label,"Real Value"])])
     # plt.plot(recall, precision, label='PR Curve')
     # plt.axhline(y = percentile, color = 'r', linestyle = '--')
-    # ax.legend(['precision pecall purve', 'percentage of positive class'])
+    # ax.legend(['PR curve', 'percentage of positive class'])
     # plt.xlabel('Recall')
     # plt.ylabel('Precision')
-    # plt.title('Precision-Recall Curve')
-    # plt.savefig("test pr")
+    # plt.title('Precision - Recall Curve')
+    # plt.savefig("precision_recall/pr_"+label+'.png')
+    # plt.close()
 
-    true_preds = x
-    false_preds = y
+    # # thresholds
+    # thresholds = np.arange(0.1, 1.0001, 0.0001)
+    # true_1_counts = []
+    # true_0_counts = []
+    # true_ones = []
+    # false_ones = []
+    # for threshold in thresholds:
+    #     pred_true_thresh = np.round(x >= threshold)
+    #     pred_false_thresh = np.round(y >= threshold)
+    #     zeros_true = np.sum(pred_true_thresh == 0)
+    #     ones_true = np.sum(pred_true_thresh == 1)
+    #     zeros_false = np.sum(pred_false_thresh == 0)
+    #     ones_false = np.sum(pred_false_thresh == 1)
+    #     true_ones.append(ones_true)
+    #     false_ones.append(ones_false)
+    # plt.plot(thresholds, true_ones, label='correct toxic classification')
+    # plt.plot(thresholds, false_ones, label='calse toxic classification')
+    # plt.title('Amount of assigned toxic labels on different thresholds')
+    # plt.xlabel('Threshold')
+    # plt.ylabel('Count')
+    # plt.xticks(np.arange(0.1,1.1,0.1))
+    # plt.legend()
+    # plt.savefig('threshold_plots/threshold_'+label+'.png')
+    # plt.close()
 
-    # set the number of rows and columns in the plot
-    nrows = 5
-    ncols = 2
+    # # overlapping hist
+    # sns.histplot(bins=20, binrange=[0,1], data=res[label][~np.isnan(res[label, "Real Value"])], palette="Set2", x="Prediction", hue="Real Value", log_scale=(False,True))
+    # plt.title('Amount of predictions dependent on value in log scale')
+    # plt.ylabel('log(Count)')
+    # plt.savefig('overlap_hist/ohist_'+label+'.png')
+    # plt.close()
 
-    # set the thresholds and corresponding titles for each plot
-    thresholds = np.arange(0.1, 1.1, 0.1)
-    titles = [f'Threshold {threshold:.1f}' for threshold in thresholds]
-
-    # create the subplots
-    fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(10, 20))
-    axs = axs.flatten()
-
-    # loop over the thresholds and plot the histograms
-    for i, thresh in enumerate(thresholds):
-        # round the predictions based on the threshold
-        pred_true_thresh = np.round(true_preds >= thresh)
-        pred_false_thresh = np.round(false_preds >= thresh)
-        
-        # Compute counts of 0s and 1s
-        zeros_true = np.sum(pred_true_thresh == 0)
-        ones_true = np.sum(pred_true_thresh == 1)
-        zeros_false = np.sum(pred_false_thresh == 0)
-        ones_false = np.sum(pred_false_thresh == 1)
-        
-        # Create tick plot on current subplot
-        axs[i].bar(['True - 0', 'True - 1', 'False - 0', 'False - 1'], [zeros_true, ones_true, zeros_false, ones_false])
-        axs[i].set_title(f'Threshold: {thresh:.1f}')
-        axs[i].set_ylim(0, 1000)
-        axs[i].set_ylabel('Counts')
-
-    # Set common xlabel for all subplots
-    fig.add_subplot(111, frameon=False)
-    plt.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
-    plt.xlabel('Class')
-    plt.subplots_adjust(hspace=0.5)
-
-    # show the plot
-    plt.savefig("multi_hist.png")
-
-    break
-
-    #plt.savefig("test split")
-    #print(res[label][~np.isnan(res[label, "Real Value"])])
-    #sns.histplot(bins=20, binrange=[0,1], data=res[label][~np.isnan(res[label, "Real Value"])], palette="Set2", x="Prediction", hue="Real Value", log_scale=(False,True))
-    #plt.savefig("test sns")
-    #break
-    #plt.ylim(0,25)
-    #sns.kdeplot(data=res[label], common_norm=False, palette="Set2", x="Prediction", hue="Real Value", ax=ax)
-    #leg = plt.legend(title=label,loc='upper right', labels=['toxic','not toxic'])
-    #plt.title("KDE of " + label)
-    #leg.get_frame().set_linewidth(0.0)
-
-    ## for AUCs
-    #plt.xlim(0,1)
-    # fpr, tpr, _ = roc_curve(res[label,"Real Value"][~np.isnan(res[label,"Real Value"])], res[label,"Prediction"][~np.isnan(res[label,"Real Value"])])
-    # plt.figure()
-    # plt.plot(
-    #     fpr, tpr, 
-    #     label="ROC curve (area = {0:0.2f})".format(roc_auc_score(res[label,"Real Value"][~np.isnan(res[label,"Real Value"])], res[label,"Prediction"][~np.isnan(res[label,"Real Value"])])),
-    #     color="navy",
-    # )
-    # plt.ylabel('True Positive Rate')
-    # plt.xlabel('False Positive Rate')
-    # plt.legend(loc="lower right")
-    # plt.title("ROC-AUC of " + label)
-
-    if zoom_on:
-        axins = zoomed_inset_axes(ax, 4, loc='center right', borderpad=5)
-        mark_inset(ax, axins, loc1=3, loc2=4, fc="none", ec="0.5")
-        axins.set_xlim([0.04,0.055])
-        axins.set_ylim([0,25])
-        
-        sns.kdeplot(data=res[label], common_norm=False, palette="Set2", x="Prediction", hue="Real Value", ax=axins)
-        axins.set(ylabel=None, xlabel=None, yticklabels=[])
-        axins.xaxis.tick_top()
-        axins.legend().remove()
-    #plt.savefig(name + "_" +  label)
-print("x", sum_all_x/12)
-print("y", sum_all_y/12)
+    # for AUCs
+    fpr, tpr, _ = roc_curve(res[label,"Real Value"][~np.isnan(res[label,"Real Value"])], res[label,"Prediction"][~np.isnan(res[label,"Real Value"])])
+    plt.figure()
+    plt.plot(
+        fpr, tpr, 
+        label="ROC curve (area = {0:0.2f})".format(roc_auc_score(res[label,"Real Value"][~np.isnan(res[label,"Real Value"])], res[label,"Prediction"][~np.isnan(res[label,"Real Value"])])),
+        color="navy",
+    )
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.legend(loc="lower right")
+    plt.title("ROC-AUC of " + label)
+    plt.savefig('rocauc/roc_'+label+'.png')
+    plt.close()
